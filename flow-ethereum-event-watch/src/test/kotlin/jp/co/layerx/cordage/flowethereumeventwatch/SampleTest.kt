8c5625de00361d0a9f864eae4833078795ab1a3b
etaroid <amthgfonger.nk@gmail.com>
2020-03-02 10:24:01 +0900

    fix wip sample test

--- flow-ethereum-event-watch/src/test/kotlin/jp/co/layerx/cordage/flowethereumeventwatch/SampleTest.kt
+++ flow-ethereum-event-watch/src/test/kotlin/jp/co/layerx/cordage/flowethereumeventwatch/SampleTest.kt
@@ -1,12 +1,13 @@
 package jp.co.layerx.cordage.flowethereumeventwatch
 
-import org.assertj.core.api.Assertions.*
+import jp.co.layerx.cordage.flowethereumeventwatch.ethWrapper.SimpleStorage
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.DefaultBlockParameter
 import org.web3j.protocol.core.methods.request.EthFilter
 import org.web3j.protocol.http.HttpService
-import jp.co.layerx.cordage.flowethereumeventwatch.ethWrapper.SimpleStorage
+
 
 class SampleTest {
     @Test
@@ -14,26 +15,34 @@ class SampleTest {
         val ETHEREUM_RPC_URL = "http://localhost:8545"
         val web3 = Web3j.build(HttpService(ETHEREUM_RPC_URL))
 
-        val fromBlockNumber = 1.toBigInteger()
-        val toBlockNumber = 50.toBigInteger()
-        val targetContractAddress = SimpleStorage.getPreviouslyDeployedAddress("15777")
-        val event = SimpleStorage.SET_EVENT.name
+//        val fromBlockNumber = 1.toBigInteger()
+//        val toBlockNumber = 50.toBigInteger()
+        val targetContractAddress = SimpleStorage.getPreviouslyDeployedAddress("5777")
+        val event = SimpleStorage.SET_EVENT
 
-//        val json = File("./SimpleStorage.json")
-//        val mapper = jacksonObjectMapper()
-//        val truffleArtifacts = mapper.readValue<TruffleArtifacts>(json)
+//        val filter = EthFilter(DefaultBlockParameter.valueOf(fromBlockNumber),
+//                DefaultBlockParameter.valueOf(toBlockNumber),
+//                targetContractAddress)
+        val filter = EthFilter(DefaultBlockParameter.valueOf("earliest"),
+                DefaultBlockParameter.valueOf("latest"),
+                targetContractAddress?.substring(2))
+//        filter.addSingleTopic(EventEncoder.encode(event))
+//        filter.addOptionalTopics("0x" + TypeEncoder.encode(new Address("0x00a329c0648769a73afac7f9381e08fb43dbea72")))
 
-        val filter = EthFilter(DefaultBlockParameter.valueOf(fromBlockNumber),
-                DefaultBlockParameter.valueOf(toBlockNumber),
-                targetContractAddress).addSingleTopic(event)
-//        filter.addSingleTopic(EventEncoder.encode(SETTLED_EVENT));
-//        filter.addOptionalTopics("0x" + TypeEncoder.encode(new Address("0x00a329c0648769a73afac7f9381e08fb43dbea72")));
+        val ethLogs = web3.ethGetLogs(filter).send()
+        val sendEventLog = ethLogs.result
 
-        val subscription = web3.ethLogFlowable(filter).subscribe { log ->
-            print(log.data)
-        }
-        subscription.dispose()
 
-        assertThat(1).isEqualTo(1)
+//        web3.ethLogObservable(filter).subscribe(object : Action1<Log?> {
+//            fun call(log: Log) {
+//                System.out.println("log.toString(): " + log.toString())
+//            }
+//        })
+
+//        val subscription = web3.ethLogFlowable(filter).subscribe { log ->
+//            val test = log.data as Nothing?
+//        }
+//        subscription.dispose()
+        assertThat(2).isEqualTo(1)
     }
 }
