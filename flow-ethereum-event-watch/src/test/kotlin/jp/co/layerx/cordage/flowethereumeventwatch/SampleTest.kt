7efb5e3fdb5b4483ae79816e909937e921fc26d7
etaroid <amthgfonger.nk@gmail.com>
2020-03-02 19:47:13 +0900

    fix sampleTest for event get

--- flow-ethereum-event-watch/src/test/kotlin/jp/co/layerx/cordage/flowethereumeventwatch/SampleTest.kt
+++ flow-ethereum-event-watch/src/test/kotlin/jp/co/layerx/cordage/flowethereumeventwatch/SampleTest.kt
@@ -3,9 +3,11 @@ package jp.co.layerx.cordage.flowethereumeventwatch
 import jp.co.layerx.cordage.flowethereumeventwatch.ethWrapper.SimpleStorage
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
+import org.web3j.abi.FunctionReturnDecoder
 import org.web3j.protocol.Web3j
 import org.web3j.protocol.core.DefaultBlockParameter
 import org.web3j.protocol.core.methods.request.EthFilter
+import org.web3j.protocol.core.methods.response.Log
 import org.web3j.protocol.http.HttpService
 
 
@@ -17,7 +19,8 @@ class SampleTest {
 
 //        val fromBlockNumber = 1.toBigInteger()
 //        val toBlockNumber = 50.toBigInteger()
-        val targetContractAddress = SimpleStorage.getPreviouslyDeployedAddress("5777")
+//        val targetContractAddress = SimpleStorage.getPreviouslyDeployedAddress("5777")
+        val targetContractAddress = "0xCfEB869F69431e42cdB54A4F4f105C19C080A601"
         val event = SimpleStorage.SET_EVENT
 
 //        val filter = EthFilter(DefaultBlockParameter.valueOf(fromBlockNumber),
@@ -25,24 +28,14 @@ class SampleTest {
 //                targetContractAddress)
         val filter = EthFilter(DefaultBlockParameter.valueOf("earliest"),
                 DefaultBlockParameter.valueOf("latest"),
-                targetContractAddress?.substring(2))
-//        filter.addSingleTopic(EventEncoder.encode(event))
-//        filter.addOptionalTopics("0x" + TypeEncoder.encode(new Address("0x00a329c0648769a73afac7f9381e08fb43dbea72")))
+                targetContractAddress)
 
         val ethLogs = web3.ethGetLogs(filter).send()
-        val sendEventLog = ethLogs.result
+        ethLogs.result.map { (it.get() as Log).data }
+                .map { FunctionReturnDecoder.decode(it, event.nonIndexedParameters) }
+        val sendEventLog =  ethLogs.result[0].get() as Log
+        val logResult = FunctionReturnDecoder.decode(sendEventLog.data, event.nonIndexedParameters)
 
-
-//        web3.ethLogObservable(filter).subscribe(object : Action1<Log?> {
-//            fun call(log: Log) {
-//                System.out.println("log.toString(): " + log.toString())
-//            }
-//        })
-
-//        val subscription = web3.ethLogFlowable(filter).subscribe { log ->
-//            val test = log.data as Nothing?
-//        }
-//        subscription.dispose()
         assertThat(2).isEqualTo(1)
     }
 }
