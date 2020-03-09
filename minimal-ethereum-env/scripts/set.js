b132c1b29d40e16bb511686ed49d0745db01227d
Shun Takagiwa <shun.takagiwa@layerx.co.jp>
2020-03-09 16:14:07 +0900

    introduce solidity linter and SimpleStorage test

--- minimal-ethereum-env/scripts/set.js
+++ minimal-ethereum-env/scripts/set.js
@@ -1,24 +1,24 @@
-const SimpleStorage = artifacts.require("SimpleStorage");
+const SimpleStorage = artifacts.require('SimpleStorage');
 const BN = require('bn.js');
 
 module.exports = async done => {
-    // parse new data from arguments
-    let amount;
-    if (!isNaN(parseInt(process.argv[4], 10))) {
-        amount = new BN(process.argv[4], 10)
-    }
+  // parse new data from arguments
+  let amount;
+  if (!isNaN(parseInt(process.argv[4], 10))) {
+    amount = new BN(process.argv[4], 10);
+  }
 
-    // get previous data
-    const simpleStorage = await SimpleStorage.deployed();
-    const before = await simpleStorage.get();
-    console.log("Previous data:", before.toString(10));
+  // get previous data
+  const simpleStorage = await SimpleStorage.deployed();
+  const before = await simpleStorage.get();
+  console.log('Previous data:', before.toString(10));
 
-    // update with new data
-    if (!amount) {
-        amount = before.add(new BN("1", 10))
-    }
-    await simpleStorage.set(amount);
-    const newData = await simpleStorage.get();
-    console.log("New data:", newData.toString(10));
-    done();
+  // update with new data
+  if (!amount) {
+    amount = before.add(new BN('1', 10));
+  }
+  await simpleStorage.set(amount);
+  const newData = await simpleStorage.get();
+  console.log('New data:', newData.toString(10));
+  done();
 };
