5f82885962de009835f8763698e38ec8f6fda39a
Shun Takagiwa <shun.takagiwa@layerx.co.jp>
2020-03-29 10:34:00 +0900

    bytes to string test

--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/SampleTest.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/SampleTest.kt
@@ -2,62 +2,45 @@ package jp.co.layerx.cordage.crosschainatomicswap
 
 import org.assertj.core.api.Assertions
 import org.junit.Test
-import org.web3j.abi.DefaultFunctionReturnDecoder
-import org.web3j.crypto.Credentials
-import org.web3j.protocol.Web3j
-import org.web3j.protocol.core.DefaultBlockParameter
-import org.web3j.protocol.core.methods.request.EthFilter
-import org.web3j.protocol.core.methods.response.Log
-import org.web3j.protocol.http.HttpService
-import org.web3j.tx.gas.StaticGasProvider
-import java.math.BigInteger
 
 
 class SampleTest {
+    /*
+    Sample solidity code
+======================================================
+pragma solidity >=0.5.17 <0.7.0;
+
+contract StringBytesEmitter {
+    event StringBytes(bytes b);
+
+    function emitStringBytes(string memory s) public {
+        bytes memory b = abi.encode(s);
+        emit StringBytes(b);
+    }
+}
+======================================================
+     */
     @Test
-    fun `get ethereum event and send tx test`() {
-        val ETHEREUM_RPC_URL = "http://localhost:8545"
-        val web3 = Web3j.build(HttpService(ETHEREUM_RPC_URL))
+    fun `bytes to string`() {
+        val hex = "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000001c4f3d5061727469636970616e74412c4c3d4c6f6e646f6e2c433d474200000000"
+        val byteArray = hex.toUpperCase().hexStringToByteArray()
+        val actual = byteArray.toString(Charsets.UTF_8)
+        Assertions.assertThat(actual == "O=ParticipantA,L=London,C=GB")
+    }
+}
 
-        val targetContractAddress = SimpleStorage.getPreviouslyDeployedAddress("5777")
-        val event = SimpleStorage.SET_EVENT
-        val searchId = 8.toBigInteger()
 
-        val filter = EthFilter(DefaultBlockParameter.valueOf("earliest"),
-                DefaultBlockParameter.valueOf("latest"),
-                targetContractAddress)
+fun String.hexStringToByteArray() : ByteArray {
+    val hexChars = "0123456789ABCDEF"
+    val result = ByteArray(length / 2)
 
-        val ethLogs = web3.ethGetLogs(filter).send()
-        val decodedLogs = ethLogs.result?.map { (it.get() as Log).data }
-                ?.map { DefaultFunctionReturnDecoder.decode(it, event.nonIndexedParameters) }
-        if (decodedLogs != null) {
-            for (decodedLog in decodedLogs) {
-                val eventValues = decodedLog?.map { it.value as BigInteger }
-                val filteredEventValues = eventValues?.filter { e -> e == searchId }
-                if (filteredEventValues != null) {
-                    for (filteredEventValue in filteredEventValues) {
-                        val credentials = Credentials.create("0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d")
-                        val simpleStorage: SimpleStorage = SimpleStorage.load(targetContractAddress, web3, credentials, StaticGasProvider(BigInteger.valueOf(1), BigInteger.valueOf(500000)))
-                        val result = simpleStorage.set(filteredEventValue.inc()).send()
-                        val returnValue = result.transactionHash
+    for (i in 0 until length step 2) {
+        val firstIndex = hexChars.indexOf(this[i]);
+        val secondIndex = hexChars.indexOf(this[i + 1]);
 
-                        Assertions.assertThat(returnValue).startsWith("0x")
-                    }
-                }
-            }
-        }
+        val octet = firstIndex.shl(4).or(secondIndex)
+        result.set(i.shr(1), octet.toByte())
     }
 
-    @Test
-    fun `send ethereum tx test`() {
-        val ETHEREUM_RPC_URL = "http://localhost:8545"
-        val web3 = Web3j.build(HttpService(ETHEREUM_RPC_URL))
-
-        val credentials = Credentials.create("0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d")
-        val simpleStorage: SimpleStorage = SimpleStorage.load("0xCfEB869F69431e42cdB54A4F4f105C19C080A601", web3, credentials, StaticGasProvider(BigInteger.valueOf(1), BigInteger.valueOf(500000)))
-        val result = simpleStorage.set(3.toBigInteger()).send()
-        val returnValue = result.transactionHash
-
-        Assertions.assertThat(returnValue).startsWith("0x")
-    }
+    return result
 }
