06258aef0c38958d2fa51a2f22f59ed58c997d6d
etaroid <amthgfonger.nk@gmail.com>
2020-04-01 16:00:44 +0900

    refactor Party.ethAddress

--- cross-chain-atomic-swap-cordapp/src/main/kotlin/jp/co/layerx/cordage/crosschainatomicswap/Utils.kt
+++ cross-chain-atomic-swap-cordapp/src/main/kotlin/jp/co/layerx/cordage/crosschainatomicswap/Utils.kt
@@ -1,5 +1,6 @@
 package jp.co.layerx.cordage.crosschainatomicswap
 
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.identity.Party
 
 private val HEX_CHARS = "0123456789ABCDEF".toCharArray()
@@ -18,23 +19,28 @@ fun ByteArray.toHex() : String {
     return result.toString()
 }
 
+private val notary = CordaX500Name("Notary", "London", "GB")
+private val participantA = CordaX500Name("ParticipantA", "London", "GB")
+private val participantB = CordaX500Name("ParticipantB", "New York", "US")
+private val participantC = CordaX500Name("ParticipantC", "Paris", "FR")
+
 fun Party.ethAddress(): String {
-    return when (this.name.organisation) {
-        // TODO import Ethereum Address from .env
-        "ParticipantA" -> {
+    return when (this.name) {
+        // TODO import Ethereum Addresses from .env
+        participantA -> {
             "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0"
         }
-        "ParticipantB" -> {
+        participantB -> {
             "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b"
         }
-        "ParticipantC" -> {
+        participantC -> {
             "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
         }
-        "Notary" -> {
+        notary -> {
             "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d"
         }
         else -> {
-            ""
+            throw IllegalArgumentException("This party's ethAddress doesn't exist.")
         }
     }
 }
