89d26908ba295d0bcd4a66a8f1f2dfd9935bd1fe
etaroid <amthgfonger.nk@gmail.com>
2020-04-03 18:12:11 +0900

    add every StateTest

--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/state/ProposalStateTest.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/state/ProposalStateTest.kt
@@ -4,6 +4,7 @@ import jp.co.layerx.cordage.crosschainatomicswap.ALICE
 import jp.co.layerx.cordage.crosschainatomicswap.BOB
 import jp.co.layerx.cordage.crosschainatomicswap.CHARLIE
 import jp.co.layerx.cordage.crosschainatomicswap.types.ProposalStatus
+import net.corda.core.contracts.UniqueIdentifier
 import org.assertj.core.api.Assertions
 import org.junit.Test
 import java.util.*
@@ -12,7 +13,18 @@ import java.util.*
 class ProposalStateTest {
     private val security = SecurityState(100, BOB.party, CHARLIE.party, "R3")
     private val expectedUuid = UUID.randomUUID()
-    private val actual = ProposalState(security, 1_000_000.toBigInteger(), "1", ALICE.party, BOB.party, "", "")
+    private val actual = ProposalState(
+        security.linearId,
+        security.amount.toBigInteger(),
+        1_000_000.toBigInteger(),
+        "1",
+        ALICE.party,
+        BOB.party,
+        "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
+        "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
+        ProposalStatus.PROPOSED,
+        UniqueIdentifier(id = expectedUuid)
+    )
 
     @Test
     fun securityLinearId() {
@@ -21,42 +33,52 @@ class ProposalStateTest {
 
     @Test
     fun securityAmount() {
+        Assertions.assertThat(actual.securityAmount == security.amount.toBigInteger())
     }
 
     @Test
     fun weiAmount() {
+        Assertions.assertThat(actual.weiAmount == 1_000_000.toBigInteger())
     }
 
     @Test
     fun swapId() {
+        Assertions.assertThat(actual.swapId == "1")
     }
 
     @Test
     fun proposer() {
+        Assertions.assertThat(actual.proposer == ALICE.party)
     }
 
     @Test
     fun acceptor() {
+        Assertions.assertThat(actual.acceptor == BOB.party)
     }
 
     @Test
     fun fromEthereumAddress() {
+        Assertions.assertThat(actual.fromEthereumAddress == "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0")
     }
 
     @Test
     fun toEthereumAddress() {
+        Assertions.assertThat(actual.toEthereumAddress == "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b")
     }
 
     @Test
     fun status() {
+        Assertions.assertThat(actual.status == ProposalStatus.PROPOSED)
     }
 
     @Test
     fun linearId() {
+        Assertions.assertThat(actual.linearId == UniqueIdentifier(id = expectedUuid))
     }
 
     @Test
     fun participants() {
+        Assertions.assertThat(actual.participants == setOf(ALICE.party, BOB.party))
     }
 
     @Test
--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/state/SecurityStateTest.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/state/SecurityStateTest.kt
@@ -2,6 +2,7 @@ package jp.co.layerx.cordage.crosschainatomicswap.state
 
 import jp.co.layerx.cordage.crosschainatomicswap.ALICE
 import jp.co.layerx.cordage.crosschainatomicswap.BOB
+import jp.co.layerx.cordage.crosschainatomicswap.CHARLIE
 import net.corda.core.contracts.UniqueIdentifier
 import org.assertj.core.api.Assertions
 import org.junit.Test
@@ -13,18 +14,22 @@ class SecurityStateTest {
 
     @Test
     fun amount() {
+        Assertions.assertThat(actual.amount == 100)
     }
 
     @Test
     fun owner() {
+        Assertions.assertThat(actual.owner == ALICE.party)
     }
 
     @Test
     fun issuer() {
+        Assertions.assertThat(actual.issuer == BOB.party)
     }
 
     @Test
     fun name() {
+        Assertions.assertThat(actual.name == "R3")
     }
 
     @Test
@@ -39,5 +44,9 @@ class SecurityStateTest {
 
     @Test
     fun withNewOwner() {
+        val securityWithNewOwner = actual.withNewOwner(CHARLIE.party)
+        val expected = actual.copy(owner = CHARLIE.party)
+
+        Assertions.assertThat(securityWithNewOwner == expected)
     }
 }
