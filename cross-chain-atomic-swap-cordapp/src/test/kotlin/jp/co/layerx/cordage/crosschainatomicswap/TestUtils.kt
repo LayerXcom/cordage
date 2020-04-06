3c2e88d8f03bf325171c12574a7afe35c3786c12
Shun Takagiwa <shun.takagiwa@layerx.co.jp>
2020-04-06 10:19:10 +0900

    use Corda testing

--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
@@ -1,8 +1,10 @@
 package jp.co.layerx.cordage.crosschainatomicswap
 
-import net.corda.core.identity.CordaX500Name
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.core.BOB_NAME
+import net.corda.testing.core.CHARLIE_NAME
 import net.corda.testing.core.TestIdentity
 
-val ALICE = TestIdentity(CordaX500Name(organisation = "Alice", locality = "TestLand", country = "US"))
-val BOB = TestIdentity(CordaX500Name(organisation = "Bob", locality = "TestCity", country = "US"))
-var CHARLIE = TestIdentity(CordaX500Name(organisation = "Charlie", locality = "TestVillage", country = "US"))
+val ALICE = TestIdentity(ALICE_NAME)
+val BOB = TestIdentity(BOB_NAME)
+var CHARLIE = TestIdentity(CHARLIE_NAME)
--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/flow/SecurityIssueFlowTest.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/flow/SecurityIssueFlowTest.kt
@@ -4,6 +4,9 @@ import jp.co.layerx.cordage.crosschainatomicswap.contract.SecurityContract
 import jp.co.layerx.cordage.crosschainatomicswap.state.SecurityState
 import net.corda.core.identity.CordaX500Name
 import net.corda.core.utilities.getOrThrow
+import net.corda.testing.core.ALICE_NAME
+import net.corda.testing.core.BOB_NAME
+import net.corda.testing.core.CHARLIE_NAME
 import net.corda.testing.node.MockNetwork
 import net.corda.testing.node.MockNetworkNotarySpec
 import net.corda.testing.node.MockNodeParameters
@@ -25,9 +28,9 @@ class SecurityIssueFlowTest {
             listOf("jp.co.layerx.cordage.crosschainatomicswap"),
             notarySpecs = listOf(MockNetworkNotarySpec(CordaX500Name("Notary", "London", "GB")))
         )
-        a = network.createNode(MockNodeParameters(legalName = CordaX500Name("Alice", "London", "GB")))
-        b = network.createNode(MockNodeParameters(legalName = CordaX500Name("Bob", "London", "GB")))
-        c = network.createNode(MockNodeParameters(legalName = CordaX500Name("Charlie", "London", "GB")))
+        a = network.createNode(MockNodeParameters(legalName = ALICE_NAME))
+        b = network.createNode(MockNodeParameters(legalName = BOB_NAME))
+        c = network.createNode(MockNodeParameters(legalName = CHARLIE_NAME))
         val startedNodes = arrayListOf(a, b, c)
         startedNodes.forEach { it.registerInitiatedFlow(SecurityIssueFlowResponder::class.java) }
         network.runNetwork()
@@ -47,9 +50,8 @@ class SecurityIssueFlowTest {
         val flow = SecurityIssueFlow(amount, owner,securityName)
         val future = c.startFlow(flow)
         network.runNetwork()
-        val response = future.getOrThrow()
+        val actualSignedTx = future.getOrThrow()
 
-        val actualSignedTx = response
         val expected = SecurityState(amount, owner, issuer, securityName)
         Assertions.assertThat(actualSignedTx.inputs.isEmpty())
         Assertions.assertThat(actualSignedTx.tx.outputStates.single() == expected)
