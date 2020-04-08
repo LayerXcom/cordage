b65ffa132d7a9c837776fa1ae4c259fb4bfe292d
etaroid <amthgfonger.nk@gmail.com>
2020-04-08 20:06:12 +0900

    add SecurityContractTransferTest

--- cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
+++ cross-chain-atomic-swap-cordapp/src/test/kotlin/jp/co/layerx/cordage/crosschainatomicswap/TestUtils.kt
@@ -3,8 +3,10 @@ package jp.co.layerx.cordage.crosschainatomicswap
 import net.corda.testing.core.ALICE_NAME
 import net.corda.testing.core.BOB_NAME
 import net.corda.testing.core.CHARLIE_NAME
+import net.corda.testing.core.DUMMY_BANK_A_NAME
 import net.corda.testing.core.TestIdentity
 
 val ALICE = TestIdentity(ALICE_NAME)
 val BOB = TestIdentity(BOB_NAME)
-var CHARLIE = TestIdentity(CHARLIE_NAME)
+val CHARLIE = TestIdentity(CHARLIE_NAME)
+val DUMMY_BANK_A = TestIdentity(DUMMY_BANK_A_NAME)
